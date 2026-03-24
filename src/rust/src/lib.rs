cf6204b7f7e09161fc39ef3b81dae263a3bc63ff
AntoinePrv <AntoinePrv@users.noreply.github.com>
2026-03-24 15:14:42 +0100

    Uniform Doc interface

--- src/rust/src/lib.rs
+++ src/rust/src/lib.rs
@@ -59,9 +59,9 @@ impl Transaction {
     fn new(doc: ExternalPtr<Doc>, #[extendr(default = "FALSE")] mutable: bool) -> Self {
         // Safety: Doc live in R memory and is kept alive in the owner field of this struct
         let transaction: DynTransaction<'static> = if mutable {
-            unsafe { DynTransaction::Write(std::mem::transmute(doc.doc.transact_mut())) }
+            unsafe { DynTransaction::Write(std::mem::transmute(doc.0.transact_mut())) }
         } else {
-            unsafe { DynTransaction::Read(std::mem::transmute(doc.doc.transact())) }
+            unsafe { DynTransaction::Read(std::mem::transmute(doc.0.transact())) }
         };
         Transaction {
             owner: doc.into(),
@@ -200,36 +200,46 @@ impl TextRef {
 }
 
 #[extendr]
-struct Doc {
-    doc: yrs::Doc,
+struct Doc(yrs::Doc);
+
+impl From<yrs::Doc> for Doc {
+    fn from(value: yrs::Doc) -> Self {
+        Self(value)
+    }
+}
+
+impl std::ops::Deref for Doc {
+    type Target = yrs::Doc;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
 }
 
 #[extendr]
 impl Doc {
     fn new() -> Self {
-        Self {
-            doc: yrs::Doc::new(),
-        }
+        Self(yrs::Doc::new())
     }
 
     fn client_id(&self) -> u64 {
-        self.doc.client_id()
+        self.0.client_id()
     }
 
     fn guid(&self) -> Strings {
-        (*self.doc.guid()).into()
+        (*self.0.guid()).into()
     }
 
     fn get_or_insert_text(&self, name: &str) -> TextRef {
-        self.doc.get_or_insert_text(name).into()
+        self.0.get_or_insert_text(name).into()
     }
 
     fn get_or_insert_map(&self, name: &str) -> MapRef {
-        self.doc.get_or_insert_map(name).into()
+        self.0.get_or_insert_map(name).into()
     }
 
     fn get_or_insert_array(&self, name: &str) -> ArrayRef {
-        self.doc.get_or_insert_array(name).into()
+        self.0.get_or_insert_array(name).into()
     }
 }
 
